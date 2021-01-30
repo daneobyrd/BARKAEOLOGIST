0439df9eed747fc6ddef6b85c6691fa5254c7e30
John Harger <john.harger@gmail.com>
2021-01-29 19:33:12 -0600

    Add simple music player

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicPlayer : MonoBehaviour {
+    [Tooltip("The track to play. This should loop.")] [SerializeField]
+    private AudioClip track;
+
+    private AudioSource _audioSource;
+
+    private void Awake() {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Start() {
+        _audioSource.clip = track;
+        _audioSource.loop = true;
+        _audioSource.Play();
+    }
+}
